e194013180184a660caf81d381296980eebffd3a
EvgeniusL <evgeniy.od.ua@gmail.com>
2014-03-25 23:25:48 +0200

    Generating textures, loading from bundle.

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class MainScript : MonoBehaviour {
 	private int _points;
 	static public int points { get; set; }
+	static public ArrayList sprites;
 	// Use this for initialization
 	public enum GameStates	//States of game
 	{
@@ -12,8 +13,18 @@ public class MainScript : MonoBehaviour {
 		Pause,
 	}
 	static public GameStates curStates;	//current state of the game
-	void Start () 
+	IEnumerator Start () 
 	{
+		WWW www = WWW.LoadFromCacheOrDownload ("file://C:/Users/Evgenius/Documents/CircleFun/AssetBundles/sprites.unity3d", 1);
+		yield return www;
+		AssetBundle bundleSprites = www.assetBundle;
+		Object [] objects = bundleSprites.LoadAll ();
+		sprites = new ArrayList ();
+		foreach (Object spr in objects) 
+		{
+			if(spr as Sprite)
+				sprites.Add(spr);
+		}
 		points = 0;
 		curStates = GameStates.Menu;
 	}
--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -8,21 +8,17 @@ public class Planet : MonoBehaviour
 	private float _size;
 	private Vector3 _destination;
 	private Vector3 _startPosition;
-
-	public int points { get; set; }			//Points for destruction
+	private SpriteRenderer sprRend;				//Sprite component
+	
+	public int points { get; set; }				//Points for destruction
 	public Vector3 speed { get; set; }			//Speed of falling
-	public float size //Size of planet
-	{ 	
-		get{ return _size; } 
-		set
-		{
-			_size = value;
-		} 
-	}			
-	public Vector3 destination { get; set; }   //Final destination
-	public Vector3 startPosition { get; set; } //StartPosition
+	public float size { get; set; }				//Size of planet
+	public Vector3 destination { get; set; }   	//Final destination
+	public Vector3 startPosition { get; set; } 	//StartPosition
+
 	void Start()
 	{
+		sprRend = GetComponent<SpriteRenderer> ();
 		size = Random.Range(0.25f, 1f);		//Set size
 		transform.localScale *= size;		//Change size
 		points = 100 - (int)(size * 100);	//Set amount of points
@@ -42,6 +38,7 @@ public class Planet : MonoBehaviour
 	}
 	void ReConstruct()	//Object pool
 	{
+		sprRend.sprite = MainScript.sprites [Random.Range (0, MainScript.sprites.Count)] as Sprite;
 		size = Random.Range(0.25f, 1f);		//Set size
 		transform.localScale *= size;		//Change size
 		points = 100 - (int)(size * 100);	//Set amount of points
